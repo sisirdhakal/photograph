68a48ea83cf3d2e063f34b4011c4f0b45a30708a
sisirdhakal <dhakalsaurav023@gmail.com>
2023-04-26 18:57:15 +0545

    google font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,32 +15,9 @@ function MyApp({ Component, pageProps }: AppProps) {
     <div className={poppins.className} >
       <Layout>
         <Component {...pageProps} />
-        {/* <ToastContainer /> */}
       </Layout>
     </div>
   </>
 }
 
 export default MyApp
-
-
-
-// import '@/styles/globals.css'
-// import Layout from '@/views/Layout'
-// import type { AppProps } from 'next/app'
-// import { Poppins } from 'next/font/google'
-
-// const poppins = Poppins({
-//   weight: ['200', '300', '400', '500', '600', '700'],
-//   subsets: ['latin'],
-// });
-
-
-// export default function App({ Component, pageProps }: AppProps) {
-//   < main className={poppins.className} >
-    // <Layout>
-    //   <Component {...pageProps} />
-    //   {/* <ToastContainer /> */}
-    // </Layout>
-//   </main >
-// }
